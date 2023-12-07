86db28462cfd85133ffc0c452e9ae70e0bb04003
Sylvino Prevot <111619132+sylvininholol@users.noreply.github.com>
2023-12-07 03:52:05 -0300

    Update testes.py

--- python/Teste/testes.py
+++ python/Teste/testes.py
@@ -10,8 +10,35 @@ import re
 
 pytesseract.pytesseract.tesseract_cmd = 'C:\Tesseract\Tesseract.exe'
 
-img = cv2.imread("alogalera.jpg")
-showImageGrid(img, "Função")
+
+
+
+"""img = cv2.imread("fontedistorcida.jpg")
+cv2.imshow("Original", img)
+cv2.waitKey(0)                                  
+img = mediana(img)                  Fonte Distorcida, fim
+img = simple_threshold(img, 160)
+img = ler_imagem(img)
+pos_processamento(img)"""
+
+
+
+
+"""img = cv2.imread("pixelgame.jpg")
+cv2.imshow("Original", img)
+cv2.waitKey(0)
+resultado = ler_imagem(img)
+pos_processamento(resultado)"""
+
+img = cv2.imread("RaioXLegal.jpg")
+cv2.imshow("principal", img)
+cv2.waitKey(0)
+resultado = ler_imagem(img)
+localizar_texto_imagem(resultado)
+
+#img = blur(img, 5)
+#img = sharp(img)
+ 
 
 
 
